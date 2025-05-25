001ca7ceb3fc40d2420139928866564d4a67f07a
Le Vy Cao <levycaoesc14@gmail.com>
2025-05-25 14:41:58 +0700

    fix the static render error

---
+++ backend/blog/src/main/java/io/springApp/blog/config/StaticFileServiceConfig.java
@@ -0,0 +1,15 @@
+package io.springApp.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticFileServiceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:uploads/images/");
+    }
+}
